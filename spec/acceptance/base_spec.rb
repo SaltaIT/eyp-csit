e987f59192c20e0ecbf829de8800e6abb733730d
Jordi Prats <jordi.prats@gmail.com>
2019-03-19 14:34:01 +0100

    acceptance

--- spec/acceptance/base_spec.rb
+++ spec/acceptance/base_spec.rb
@@ -8,13 +8,64 @@ describe 'csit class' do
     it 'should work with no errors' do
       pp = <<-EOF
 
-      class { 'csit': }
+      csit::pkg { 'csitdemo':
+        content => file('csit/demo/csitdemo-3.14.15_926_53_59-1.noarch.rpm'),
+      }
 
       EOF
 
       # Run it twice and test for idempotency
       expect(apply_manifest(pp).exit_code).to_not eq(1)
       expect(apply_manifest(pp).exit_code).to eq(0)
+    end
+
+    describe package('csitdemo') do
+      it { is_expected.to be_installed }
+    end
+
+    it 'should work with no errors' do
+      pp = <<-EOF
+
+      csit::pkg { 'csitdemo':
+      ensure => 'absent',
+        content => file('csit/demo/csitdemo-3.14.15_926_53_59-1.noarch.rpm'),
+      }
+
+      EOF
+
+      # Run it twice and test for idempotency
+      expect(apply_manifest(pp).exit_code).to_not eq(1)
+      expect(apply_manifest(pp).exit_code).to eq(0)
+    end
+
+    it "csit cleanup" do
+      expect(shell("rm -f /usr/local/src/*").exit_code).to be_zero
+    end
+
+    it 'should work with no errors' do
+      pp = <<-EOF
+
+      csit::pkg { 'csitdemo':
+        content => file('csit/demo/csitdemo-3.14.15_926_53_59-1.noarch.rpm'),
+      }
+
+      csit::preinstallscript { 'csitdemo':
+        content => file('csit/demo/preinstalldemo.sh'),
+      }
+
+      EOF
+
+      # Run it twice and test for idempotency
+      expect(apply_manifest(pp).exit_code).to_not eq(1)
+      expect(apply_manifest(pp).exit_code).to eq(0)
+    end
+
+    describe package('csitdemo') do
+      it { is_expected.to be_installed }
+    end
+
+    describe file('/usr/local/src/preinstall-csitdemo.installed') do
+      it { should be_file }
     end
 
   end
