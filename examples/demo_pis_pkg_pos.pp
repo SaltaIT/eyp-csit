01ef63a2cf718aa7eee203c4526ac285e3a47625
Jordi Prats <jordi.prats@gmail.com>
2019-03-19 14:44:58 +0100

    pis pos

---
+++ examples/demo_pis_pkg_pos.pp
@@ -0,0 +1,12 @@
+
+csit::pkg { 'csitdemo':
+  content => file('csit/demo/csitdemo-3.14.15_926_53_59-1.noarch.rpm'),
+}
+
+csit::preinstallscript { 'csitdemo':
+  content => file('csit/demo/preinstalldemo.sh'),
+}
+
+csit::postinstallscript { 'csitdemo':
+  content => file('csit/demo/postinstalldemo.sh'),
+}
