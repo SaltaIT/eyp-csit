d7699b160435c55c861bd00778a56429919fb2f6
Jordi Prats <jordi.prats@gmail.com>
2019-03-25 16:49:52 +0100

    01.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 0.1.1
+
+* improved csit::pkg compatibility with hiera
+
 ## 0.1.0
 
 * initial release
--- manifests/pkg.pp
+++ manifests/pkg.pp
@@ -23,31 +23,21 @@ define csit::pkg(
     }
   }
 
-  if($content!=undef)
-  {
-    file { "${::csit::srcdir}/pkg-${pkgname}.${repoprovider}":
-      ensure  => 'present',
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0640',
-      content => $content,
-      require => Class['::csit'],
-    }
-
-    package { $pkgname:
-      ensure   => $ensure,
-      source   => "${::csit::srcdir}/pkg-${pkgname}.${repoprovider}",
-      provider => $repoprovider,
-      require  => File["${::csit::srcdir}/pkg-${pkgname}.${repoprovider}"],
-    }
+  file { "${::csit::srcdir}/pkg-${pkgname}.${repoprovider}":
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    content => $content,
+    source  => $source,
+    require => Class['::csit'],
   }
-  else
-  {
-    package { $pkgname:
-      ensure   => $ensure,
-      source   => $source,
-      provider => $provider,
-    }
+
+  package { $pkgname:
+    ensure   => $ensure,
+    source   => "${::csit::srcdir}/pkg-${pkgname}.${repoprovider}",
+    provider => $repoprovider,
+    require  => File["${::csit::srcdir}/pkg-${pkgname}.${repoprovider}"],
   }
 
   Exec <| tag == "preinstallscript-${pkgname}" |> ->
